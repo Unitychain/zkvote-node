5b756b7d23eaa83803a927d2d257f075d394bb7a
Juin Chiu <davidjuin0519@gmail.com>
2019-12-05 21:17:21 +0800

    Implement data types

--- zkvote/identity.go
+++ zkvote/identity.go
@@ -2,18 +2,39 @@ package zkvote
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 )
 
 // Identity ...
 type Identity struct {
-	commitment string
+	commitment []byte
 }
 
-// // IdentityIndex ...
-// type IdentityIndex struct {
-// 	hash [32]byte
-// }
+// IdentityHash ...
+type IdentityHash struct {
+	hash []byte
+}
 
-func (i *Identity) hash() [32]byte {
-	return sha256.Sum256([]byte(i.commitment))
+// IdentityIndex ...
+type IdentityIndex struct {
+	Index map[SubjectHashHex]*IdentityHashSet
+}
+
+// IdentityHashSet ...
+type IdentityHashSet struct {
+	set map[IdentityHashHex]string
+}
+
+// IdentityHashHex ...
+type IdentityHashHex struct {
+	hex string
+}
+
+func (i *Identity) hash() *IdentityHash {
+	h := sha256.Sum256([]byte(i.commitment))
+	return &IdentityHash{hash: h[:]}
+}
+
+func (i *IdentityHash) hex() IdentityHashHex {
+	return IdentityHashHex{hex: hex.EncodeToString(i.hash)}
 }
