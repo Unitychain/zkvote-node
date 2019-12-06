15953ee429989e544c0ed174f45f34f81bd09d10
Juin Chiu <davidjuin0519@gmail.com>
2019-12-06 17:21:22 +0800

    Refactor identity

---
+++ zkvote/identity/identity.go
@@ -0,0 +1,59 @@
+package identity
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+
+	"github.com/unitychain/zkvote-node/zkvote/subject"
+)
+
+// Identity ...
+type Identity struct {
+	commitment string
+}
+
+// NewIdentity ...
+func NewIdentity(commitment string) *Identity {
+	return &Identity{commitment: commitment}
+}
+
+// Hash ...
+type Hash []byte
+
+// Byte ...
+func (h Hash) Byte() []byte { return []byte(h) }
+
+// Hex ...
+func (h Hash) Hex() HashHex {
+	return HashHex(hex.EncodeToString(h.Byte()))
+}
+
+// Index ...
+type Index map[subject.HashHex]HashSet
+
+// NewIndex ...
+func NewIndex() Index {
+	return Index(make(map[subject.HashHex]HashSet))
+}
+
+// HashSet ...
+type HashSet map[HashHex]string
+
+// NewHashSet ...
+func NewHashSet() HashSet {
+	result := HashSet(make(map[HashHex]string))
+	return result
+}
+
+// HashHex ...
+type HashHex string
+
+// String ...
+func (h HashHex) String() string { return string(h) }
+
+// Hash ...
+func (i *Identity) Hash() *Hash {
+	h := sha256.Sum256([]byte(i.commitment))
+	result := Hash(h[:])
+	return &result
+}
