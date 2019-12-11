2cc3a63f15a2d2f1669c23030c359c68e9950ad7
kimiwu123 <leonhartwu@gmail.com>
2019-12-11 16:30:29 +0800

    move proposal to subject imp

--- zkvote/subject/subject_imp.go
+++ zkvote/subject/subject_imp.go
@@ -1,10 +1,11 @@
-package voter
+package subject
 
 import (
 	"math/big"
 
 	crypto "github.com/ethereum/go-ethereum/crypto"
 	"github.com/unitychain/zkvote-node/zkvote/snark"
+	"github.com/unitychain/zkvote-node/zkvote/identity"
 	"github.com/unitychain/zkvote-node/zkvote/utils"
 )
 
@@ -23,14 +24,14 @@ type nullifier struct {
 type Proposal struct {
 	nullifiers map[int]*nullifier
 	index      int
-	members    *Identity
+	members    *IdentityImp
 }
 
 const HASH_YES = "43379584054787486383572605962602545002668015983485933488536749112829893476306"
 const HASH_NO = "85131057757245807317576516368191972321038229705283732634690444270750521936266"
 
 // NewProposal ...
-func NewProposal(identity *Identity) (*Proposal, error) {
+func NewProposal(identity *IdentityImp) (*Proposal, error) {
 
 	nullifiers := map[int]*nullifier{
 		0: &nullifier{
